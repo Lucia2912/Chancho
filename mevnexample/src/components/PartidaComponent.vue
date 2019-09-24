adff0d5deeacb87a9905267107f94d426f361171
nahuel062 <nambroa@gmail.com>
2019-09-23 22:46:47 -0300

    commit de prueba

---
+++ mevnexample/src/components/PartidaComponent.vue
@@ -0,0 +1,18 @@
+<template>
+<div v-model="container"></div>
+</template>
+
+<script>
+export default {
+ data(){
+   return{
+     container:{}
+   };
+ },
+ created(){
+   let deck = this.$cardeame().Deck();
+   deck.mount(this.container);
+ }
+}
+</script>
+
